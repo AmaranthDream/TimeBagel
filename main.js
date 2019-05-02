eaa81b5df1d74d4f5118f381693d366b2e02c086
Amaranth the Tiger <dreamamaranth@gmail.com>
2019-05-02 04:49:30 +0300

    backstep

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const options = {
   beginKey: "begin",
   endKey: "end",
-  radius: 50,
+  radius: 100,
   color: "#ffc007",
   showControl: true
 }
@@ -24,116 +24,118 @@ function detectCollision(data) {
   }
 }
 
+///Returns data for build diagrams divided by days
+function splitDays(obj) {
+  const beginDate = new Date(obj[options.beginKey]);
+  const endDate = new Date(obj[options.endKey]);
+  const dateToDegree = (date) => {
+    return Date.parse("Thu Jan 01 1970 " + date.toLocaleTimeString() + " GMT+0000") * (360 / 86400000)
+  }
+  if (beginDate.toDateString() == endDate.toDateString()) {
+    return [{
+      group: beginDate.toLocaleDateString(),
+      beginAngle: dateToDegree(beginDate),
+      beginLabel: beginDate.toLocaleTimeString(),
+      endAngle: dateToDegree(endDate),
+      endLabel: endDate.toLocaleTimeString()
+    }]
+  } else {
+    const tempEndDate = new Date(beginDate);
+    tempEndDate.setHours(23, 59, 59, 999);
+    return [{
+        group: beginDate.toLocaleDateString(),
+        beginAngle: dateToDegree(beginDate),
+        beginLabel: beginDate.toLocaleTimeString(),
+        endAngle: dateToDegree(tempEndDate),
+        endLabel: tempEndDate.toLocaleTimeString()
+      },
+      splitDays({
+        [`${options.beginKey}`]: parseInt(`${tempEndDate.getTime()+1}`),
+        [`${options.endKey}`]: parseInt(`${obj[options.endKey]}`)
+      })
+    ].flat();
+  }
+}
+///Checking element for errors
+function errorChecking(element) {
+  if (!(
+      options.beginKey in element &&
+      options.endKey in element &&
+      typeof element[options.beginKey] === "number" &&
+      typeof element[options.endKey] === "number"
+    )) {
+    console.log("Error format for " + element);
+    return false;
+  }
+  if (element[options.beginKey] > element[options.endKey]) {
+    console.log("Error time data for " + element);
+    return false;
+  }
+  return true;
+}
+///Group array by "group".
+function group(rv, x) {
+  (rv[x["group"]] = rv[x["group"]] || []).push(x);
+  delete x["group"];
+  return rv;
+}
+///compareFunction for sorting
+function compareDates(a, b) {
+  function parseDate(str) {
+    let parts = str.match(/(\d+)/g);
+    return parseInt(parts[2] + parts[1] + parts[0]);
+  }
+  return parseDate(a[0]) - parseDate(b[0])
+}
+///Transform raw data and returns data to build diagrams.
+function remold(data) {
+  return Object.entries(
+    data.filter(errorChecking).map(splitDays).flat().reduce(group, [])).
+  sort(compareDates);
+}
+///Converts polar coordinates to cartesian
+function polarToCartesian(ox, oy, r, Phi) {
+  return {
+    x: ox + (r * Math.cos((Phi - 90) * Math.PI / 180.0)),
+    y: oy + (r * Math.sin((Phi - 90) * Math.PI / 180.0))
+  };
+}
+///Returns SVG path
+function makePath(cx, cy, radius, startAngle, endAngle, thickness, label) {
+  const start = polarToCartesian(cx, cy, radius, endAngle);
+  const end = polarToCartesian(cx, cy, radius, startAngle);
+  const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+  //var title=(new Date(TimeData[0].begin)).toLocaleTimeString()+"\n"+(new Date(TimeData[0].end)).toLocaleTimeString();
+  const cutoutRadius = radius - thickness,
+    start2 = polarToCartesian(cx, cy, cutoutRadius, endAngle),
+    end2 = polarToCartesian(cx, cy, cutoutRadius, startAngle),
+    d = [
+      "M", start.x, start.y,
+      "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y,
+      "L", cx, cy,
+      "Z",
+
+      "M", start2.x, start2.y,
+      "A", cutoutRadius, cutoutRadius, 0, largeArcFlag, 0, end2.x, end2.y,
+      "L", cx, cy,
+      "Z"
+    ].join(" ");
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute("d", d);
+  path.setAttribute("fill-rule", "evenodd");
+  path.setAttribute("fill-opacity", ".85");
+  path.setAttribute("fill", options.color);
+  const title = document.createElementNS("http://www.w3.org/2000/svg", "title");
+  title.innerHTML = label;
+  path.appendChild(title);
+  return path;
+}
+
 //create Time Bagel frow raw array in parentNodeName node.
 function createTimeBagel(parentNodeName, raw) {
-  ///Transform raw data and returns data to build diagrams.
-  function remold(data) {
-    ///Returns data for build diagrams divided by days
-    function splitDays(obj) {
-      const beginDate = new Date(obj[options.beginKey]);
-      const endDate = new Date(obj[options.endKey]);
-      const dateToDegree = (date) => {
-        return Date.parse("Thu Jan 01 1970 " + date.toLocaleTimeString() + " GMT+0000") * (360 / 86400000)
-      }
-      if (beginDate.toDateString() == endDate.toDateString()) {
-        return [{
-          group: beginDate.toLocaleDateString(),
-          beginAngle: dateToDegree(beginDate),
-          beginLabel: beginDate.toLocaleTimeString(),
-          endAngle: dateToDegree(endDate),
-          endLabel: endDate.toLocaleTimeString()
-        }]
-      } else {
-        const tempEndDate = new Date(beginDate);
-        tempEndDate.setHours(23, 59, 59, 999);
-        return [{
-            group: beginDate.toLocaleDateString(),
-            beginAngle: dateToDegree(beginDate),
-            beginLabel: beginDate.toLocaleTimeString(),
-            endAngle: dateToDegree(tempEndDate),
-            endLabel: tempEndDate.toLocaleTimeString()
-          },
-          splitDays({
-            [`${options.beginKey}`]: parseInt(`${tempEndDate.getTime()+1}`),
-            [`${options.endKey}`]: parseInt(`${obj[options.endKey]}`)
-          })
-        ].flat();
-      }
-    }
-    ///Checking element for errors
-    function errorChecking(element) {
-      if (!(
-          options.beginKey in element &&
-          options.endKey in element &&
-          typeof element[options.beginKey] === "number" &&
-          typeof element[options.endKey] === "number"
-        )) {
-        console.log("Error format for " + element);
-        return false;
-      }
-      if (element[options.beginKey] > element[options.endKey]) {
-        console.log("Error time data for " + element);
-        return false;
-      }
-      return true;
-    }
-    ///Group array by "group".
-    function group(rv, x) {
-      (rv[x["group"]] = rv[x["group"]] || []).push(x);
-      delete x["group"];
-      return rv;
-    }
-    ///compareFunction for sorting
-    function compareDates(a, b) {
-      function parseDate(str) {
-        let parts = str.match(/(\d+)/g);
-        return parseInt(parts[2] + parts[1] + parts[0]);
-      }
-      return parseDate(a[0]) - parseDate(b[0])
-    }
-    return Object.entries(
-      data.filter(errorChecking).map(splitDays).flat().reduce(group, [])).
-    sort(compareDates);
-  }
-  ///Converts polar coordinates to cartesian
-  function polarToCartesian(ox, oy, r, Phi) {
-    return {
-      x: ox + (r * Math.cos((Phi - 90) * Math.PI / 180.0)),
-      y: oy + (r * Math.sin((Phi - 90) * Math.PI / 180.0))
-    };
-  }
-  ///Returns SVG path
-  function makePath(cx, cy, radius, startAngle, endAngle, thickness, label) {
-    const start = polarToCartesian(cx, cy, radius, endAngle);
-    const end = polarToCartesian(cx, cy, radius, startAngle);
-    const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
-    //var title=(new Date(TimeData[0].begin)).toLocaleTimeString()+"\n"+(new Date(TimeData[0].end)).toLocaleTimeString();
-    const cutoutRadius = radius - thickness,
-      start2 = polarToCartesian(cx, cy, cutoutRadius, endAngle),
-      end2 = polarToCartesian(cx, cy, cutoutRadius, startAngle),
-      d = [
-        "M", start.x, start.y,
-        "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y,
-        "L", cx, cy,
-        "Z",
 
-        "M", start2.x, start2.y,
-        "A", cutoutRadius, cutoutRadius, 0, largeArcFlag, 0, end2.x, end2.y,
-        "L", cx, cy,
-        "Z"
-      ].join(" ");
-    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path.setAttribute("d", d);
-    path.setAttribute("fill-rule", "evenodd");
-    path.setAttribute("fill-opacity", ".85");
-    path.setAttribute("fill", options.color);
-    const title = document.createElementNS("http://www.w3.org/2000/svg", "title");
-    title.innerHTML = label;
-    path.appendChild(title);
-    return path;
-  }
 
-  const data=remold(raw);
+  const data = remold(raw);
   let i = 0; //current page
   const SVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   const label = document.createElementNS("http://www.w3.org/2000/svg", "text");
@@ -155,7 +157,7 @@ function createTimeBagel(parentNodeName, raw) {
   const parent = document.getElementById(parentNodeName);
   //ToDo Condition parent no null
   console.log(options);
-  SVG.setAttribute("viewBox", `0 0 ${options.radius*2} ${options.radius*2}`);
+  //SVG.setAttribute("viewBox", `0 0 ${options.radius*2} ${options.radius*2}`);
   SVG.setAttribute("width", `${options.radius*2}`);
   SVG.setAttribute("height", `${options.radius*2}`);
   SVG.setAttribute("id", "some_svg");
@@ -166,8 +168,8 @@ function createTimeBagel(parentNodeName, raw) {
   outerCircle.setAttribute("cy", options.radius);
   outerCircle.setAttribute("fill", "none");
 
-  outerCircle.setAttribute("stroke","black");
-  outerCircle.setAttribute("stroke-width",.1);
+  outerCircle.setAttribute("stroke", "black");
+  outerCircle.setAttribute("stroke-width", .1);
   SVG.appendChild(outerCircle);
 
   dataGroup.setAttribute("id", "data");
@@ -177,10 +179,10 @@ function createTimeBagel(parentNodeName, raw) {
 
   for (let i = 0; i < 24; i++) {
     let shirt = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    shirt.setAttribute("x1", polarToCartesian(options.radius, options.radius, options.radius * 0.7 + (options.radius*0.1*!!(i % 6)) , i * 15).x);
-    shirt.setAttribute("y1", polarToCartesian(options.radius, options.radius, options.radius * 0.7 + (options.radius*0.1*!!(i % 6)), i * 15).y);
-    shirt.setAttribute("x2", polarToCartesian(options.radius, options.radius, options.radius,i*15).x);// * 0.9 + (options.radius * 0.2 * (!(i % 6))), i * 15).x);
-    shirt.setAttribute("y2", polarToCartesian(options.radius, options.radius, options.radius,i*15).y);// * 0.9 + (options.radius * 0.2 * (!(i % 6))), i * 15).y);
+    shirt.setAttribute("x1", polarToCartesian(options.radius, options.radius, options.radius * 0.7 + (options.radius * 0.1 * !!(i % 6)), i * 15).x);
+    shirt.setAttribute("y1", polarToCartesian(options.radius, options.radius, options.radius * 0.7 + (options.radius * 0.1 * !!(i % 6)), i * 15).y);
+    shirt.setAttribute("x2", polarToCartesian(options.radius, options.radius, options.radius, i * 15).x); // * 0.9 + (options.radius * 0.2 * (!(i % 6))), i * 15).x);
+    shirt.setAttribute("y2", polarToCartesian(options.radius, options.radius, options.radius, i * 15).y); // * 0.9 + (options.radius * 0.2 * (!(i % 6))), i * 15).y);
     shirt.setAttribute("stroke", "black");
     shirt.setAttribute("stroke-width", options.radius * 0.005 + (options.radius * 0.005 * (!(i % 6))));
     clockFace.appendChild(shirt);
@@ -198,11 +200,11 @@ function createTimeBagel(parentNodeName, raw) {
   leftArrow.setAttribute("fill", "black");
   leftArrow.addEventListener("click", onClick = (e) => {
     i--;
-    if(i <= 0){
+    if (i <= 0) {
       leftArrow.setAttribute("visibility", "hidden");
 
-      i=0;
-    }else{
+      i = 0;
+    } else {
       rightArrow.setAttribute("visibility", "visible");
     }
     reDraw(dataGroup, data[i]);
@@ -212,17 +214,17 @@ function createTimeBagel(parentNodeName, raw) {
   rightArrow.setAttribute("fill", "black");
   rightArrow.addEventListener("click", onClick = (e) => {
     i++;
-    if(i >= data.length - 1){
+    if (i >= data.length - 1) {
       i = data.length - 1;
       rightArrow.setAttribute("visibility", "hidden");
-    }else{
+    } else {
       leftArrow.setAttribute("visibility", "visible");
     }
     reDraw(dataGroup, data[i]);
   });
 
-  (i == 0)?leftArrow.setAttribute("visibility", "hidden"):void(0);
-  (i == data.length - 1)?rightArrow.setAttribute("visibility", "hidden"):void(0);
+  (i == 0) ? leftArrow.setAttribute("visibility", "hidden"): void(0);
+  (i == data.length - 1) ? rightArrow.setAttribute("visibility", "hidden"): void(0);
   if (options.showControl) {
     SVG.appendChild(leftArrow);
     SVG.appendChild(rightArrow);
